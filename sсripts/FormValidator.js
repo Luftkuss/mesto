d57bc534f45678ded33e96c2ba428ed1291a6797
Nikita Korlokov <korolkovniki@yandex.ru>
2022-11-01 00:27:52 +0300

    add new method

--- sсripts/FormValidator.js
+++ sсripts/FormValidator.js
@@ -29,19 +29,39 @@ class FormValidator {
     this.inputList.forEach(inputElement => {
         inputElement.addEventListener('input', () => {
             this._checkInputValidity();
-            // console.log(1)
-        
         });
       });
   };
 
-  _checkInputValidity(){
-    console.log(1);
+  _checkInputValidity(inputElement){
+    console.log(inputElement)
+
+    if (!inputElement.validity.valid) {
+        showInputError(this.formElement, inputElement, inputElement.validationMessage, inputErrorClass, errorClass);
+        console.log(showInputError1)
+    } else {
+        hideInputError(this.formElement, inputElement, this.errorClass, this.inputErrorClass);
+        console.log(hideInputError1)
+    }
   }
 
 
 };
 
+// const showInputError = (formElement, inputElement, errorMessage, inputErrorClass, errorClass) => {
+//     const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+//     inputElement.classList.add(inputErrorClass);
+//     errorElement.textContent = errorMessage;
+//     errorElement.classList.add(errorClass);
+//   };
+
+// const hideInputError = (formElement, inputElement, errorClass, inputErrorClass) => {
+//     const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+//     inputElement.classList.remove(inputErrorClass);
+//     errorElement.classList.remove(errorClass);
+//     errorElement.textContent = '';
+//   };
+  
 
 
 const classSettings = {
