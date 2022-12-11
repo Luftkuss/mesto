c328b5e7d5ec7c025fae83123ee0f47598083cc7
Korolkov Nikita <korolkovniki@yandex.ru>
2022-12-11 03:08:42 +0300

    api__name

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,9 +1,31 @@
-fetch('https://mesto.nomoreparties.co/v1/cohort-54/cards', {
-  headers: {
-    authorization: 'ce774f77-8c0e-4344-a990-44b9a94c00ea'
+export default class Api {
+    constructor({url, headers}) {
+      this._url = url,
+      this._headers = headers
+    }
+
+    getUserInformation(){
+      console.log('getUserInformation')
+      return this.test = fetch("https://nomoreparties.co/v1/cohort-54/users/me", {
+          headers: this._headers
+      })
+      .then(res => res.ok ? res.json() : Promise.reject(res.status))
+      .catch(console.log)
+    }
+
+  //   получить список всех карточек в виде массива (GET)
+
+  // добавить карточку (POST)
+
+  // удалить карточку (DELETE)
+
+  // получить данные пользователя (GET) DONE
+
+  // аменить данные пользователя (PATCH)
+
+  // заменить аватар (PATCH)
+
+  // “ залайкать” карточку (PUT)
+
+  // удалить лайк карточки (DELETE)
   }
-})
-  .then(res => res.json())
-  .then((result) => {
-    console.log(result);
-  });
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,13 +9,30 @@ import { classSettings } from '../utils/scripts.js';
 import PopupWithImage from '../components/PopupWithImage.js';
 import PopupWithForm from '../components/PopupWithForm.js';
 import UserInfo from '../components/UserInfo.js';
+import Api from '../components/Api.js';
+
+const api = new Api({
+  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-54',
+  headers: {
+    authorization: 'ce774f77-8c0e-4344-a990-44b9a94c00ea',
+    'Content-Type': 'application/json'
+  }
+});
+
+api.getUserInformation()
+  .then(res => {
+    console.log('Ответ', res)
+    userInfo.setUserInfo({name: res.name, description: res.about})
+  })
+
+ 
 
 const userInfo = new UserInfo({ nameElement: profileName, descriptionElement: profileDescription });
 
 formElementEditProfile.addEventListener('submit', submitFormEditProfile);
 
 function submitFormEditProfile(editObject) {
-    userInfo.setUserInfo(editObject)
+    // userInfo.setUserInfo(editObject)
   };
 
 const рopupEditButtonElement = new PopupWithForm('.popup_edit_profile', submitFormEditProfile);
